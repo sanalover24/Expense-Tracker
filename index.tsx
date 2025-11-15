2117826a6c2213b7f0b1e5d296fbbe9624682854
sanalover24 <mahroofnihath1@gmail.com>
2025-11-15 13:39:47 +0530

    update

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <-- THIS LINE MUST EXIST
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {

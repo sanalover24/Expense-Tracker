ddbcdf2326c59e15f3456c36da78830603c46745
sanalover24 <mahroofnihath1@gmail.com>
2025-11-15 14:44:35 +0530

    update

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App'; // Path is now correct
+import App from './App'; // Path is now correct
 import './index.css'; // Path is now correct
 
 const rootElement = document.getElementById('root');

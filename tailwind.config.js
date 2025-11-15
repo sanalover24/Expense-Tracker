ade8fd122375fa569d9588bf04cff2b19051940a
sanalover24 <mahroofnihath1@gmail.com>
2025-11-15 14:26:04 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  // This is the crucial line that enables class-based dark mode
+  darkMode: 'class',
+  
+  // This tells Tailwind to scan all of your component and page files
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
